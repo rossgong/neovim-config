d481781db4c89539b468b6074d3bbca314279156
Ross Gongaware <ross@gongaware.org>
2024-12-19 00:38:31 -0500

    Add floating term

---
+++ lua/plugins/term.lua
@@ -0,0 +1,15 @@
+return {
+	'akinsho/toggleterm.nvim',
+	version = '*',
+	config = function()
+		require("toggleterm").setup {
+			open_mapping = [[<c-\>]],
+			direction = 'float',
+
+			float_opts = {
+				border = 'curved',
+			}
+			-- other configurations can go here
+		}
+	end
+}
