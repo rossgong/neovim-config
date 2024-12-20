ee19fe34adb41bf8c87908855431b20fe90cde04
Ross Gongaware <ross@gongaware.org>
2024-12-19 21:49:05 -0500

    Add lualine

--- lua/plugins/coreplugins.lua
+++ lua/plugins/coreplugins.lua
@@ -23,4 +23,11 @@ return {
 			})
 		end
 	},
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		config = function()
+			require('lualine').setup()
+		end	
+	}
 }
