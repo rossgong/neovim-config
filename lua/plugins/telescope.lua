23e1652cd6e12c69fdf70a9257e2a266e70b1f6d
Ross Gongaware <ross@gongaware.org>
2025-02-04 18:08:14 -0500

    Fix MAson filter

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,17 @@
 return {
 	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
-		dependencies = { 'nvim-lua/plenary.nvim', 'debugloop/telescope-undo.nvim' },
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.8',
+		dependencies = { 'nvim-lua/plenary.nvim', 'debugloop/telescope-undo.nvim', "nvim-telescope/telescope-ui-select.nvim" },
 		config = function()
-			require("telescope").load_extension("undo")
+			require("telescope").setup {
+				extensions = {
+					["ui-select"] = { require("telescope.themes").get_dropdown {} }
+				}
+			};
+
+			require("telescope").load_extension("ui-select");
+			require("telescope").load_extension("undo");
 		end
 	}
 }
