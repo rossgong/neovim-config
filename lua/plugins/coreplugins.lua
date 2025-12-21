8bd07a6a8bd8cc97183de91deeaf5bc70c379b20
Ross <ross@gongaware.org>
2025-12-20 21:17:01 -0500

    Update config

--- lua/plugins/coreplugins.lua
+++ lua/plugins/coreplugins.lua
@@ -13,7 +13,7 @@ return {
 		'nvim-treesitter/nvim-treesitter',
 		build = ":TSUpdate",
 		config = function()
-			local configs = require("nvim-treesitter.configs")
+			local configs = require("nvim-treesitter.config")
 
 			configs.setup({
 				ensure_installed = { "lua", "vim", "vimdoc", "query" },
