b1385684ed372b4d048060a85402ccb88238661c
Ross Gongaware <ross@gongaware.org>
2026-05-17 23:37:12 -0400

    Switch colorscheme

--- init.lua
+++ init.lua
@@ -13,6 +13,8 @@ vim.opt.guicursor = string.format("n-v-c-sm:block,i-ci-ve:block-%s,r-cr-o:hor20-
 
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.signcolumn = 'yes'
+
 
 vim.opt.wrap = false
 
@@ -29,8 +31,6 @@ vim.opt.scrolloff = 8
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
-
-vim.cmd.colorscheme('retrobox')
 
 -- LSP
 vim.lsp.config['luals'] = {
@@ -53,14 +53,6 @@ vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<cr>', { desc = "LSP Ren
 vim.keymap.set('n', 'gl', vim.diagnostic.open_float, { desc = 'Show line diagnostics' })
 vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', { desc = "LSP Reformat" })
 vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', { desc = "LSP Code Action" })
-
--- return {
--- 	{ 'neovim/nvim-lspconfig' },
--- 	{ 'hrsh7th/cmp-nvim-lsp' },
--- 	{ 'hrsh7th/nvim-cmp' },
--- 	{ 'williamboman/mason.nvim' },
--- 	{ 'williamboman/mason-lspconfig.nvim' },
--- }
 
 -- mini.ai setup
 vim.pack.add({ { src = 'https://github.com/nvim-mini/mini.ai', version = 'stable' } })
@@ -90,9 +82,7 @@ vim.pack.add({
 	'https://github.com/nvim-telescope/telescope-ui-select.nvim',
 })
 
-vim.pack.add({
-	{ src = 'https://github.com/nvim-telescope/telescope.nvim' }
-})
+vim.pack.add({ { src = 'https://github.com/nvim-telescope/telescope.nvim' } })
 
 require("telescope").setup {
 	extensions = {
@@ -110,3 +100,6 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Telescope keymaps' })
+
+vim.pack.add({ "https://github.com/rebelot/kanagawa.nvim" })
+vim.cmd.colorscheme("kanagawa-wave")
